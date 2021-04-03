6ebece5b92f6ad7235d3be369f456799c80741fa
Endzhe Galieva <endzhe91@gmail.com>
2021-04-03 19:11:58 +0300

    a different way of setting link hrefs

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -11,9 +11,17 @@ function ClientsPage() {
     <div>
       <h1>The Clients Page</h1>
       <ul>
-        {clients.map(client => <li key={client.id}>
-          <Link href={`/clients/${client.id}`}>{client.name}</Link>
-        </li>)}
+        {clients.map(client => (
+          <li key={client.id}>
+          <Link href={{
+             pathname: '/clients/[id]',
+             query: { id: client.id },
+          }}
+          >
+            {client.name}
+          </Link>
+        </li>
+        ))}
       </ul>
     </div>
   );
