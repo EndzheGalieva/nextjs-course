345956b260a139d302d1efd1f7518fef2d264039
Endzhe Galieva <endzhe91@gmail.com>
2021-04-05 21:39:31 +0300

    adding more react components and connecting components

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,11 +1,22 @@
-import EventItem from './event-item'
+import EventItem from './event-item';
 
 function EventList(props) {
   const { items } = props;
 
-  return <ul>
-    {items.map(event => <EventItem />)}
+  return (
+    <ul>
+     {items !== undefined && items.length > 0 ? items.map((event) => (
+      <EventItem
+        key={event.id}
+        id={event.id}
+        title={event.title}
+        location={event.location}
+        date={event.date}
+        image={event.image}
+      />
+    )) : null}
   </ul>
+  );
 }
 
 export default EventList;
