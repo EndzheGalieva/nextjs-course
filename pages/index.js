b4b4f21bf545a28e71c71974d08068ab367a3f8b
Endzhe Galieva <endzhe91@gmail.com>
2021-04-16 11:26:07 +0300

    starting setup and a challenge for you

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from "../components/events/event-list";
+import NewsletterRegistration from "../components/input/newsletter-registration";
 
 function HomePage(props) {
   return (
@@ -13,6 +14,7 @@ function HomePage(props) {
           content='Find a lot of great events that allow you to evolve...'
         />
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events} />
     </div>
   );
@@ -26,7 +28,7 @@ export async function getStaticProps() {
       events: featuredEvents
     },
     revalidate: 1800
-  }
+  };
 }
 
 export default HomePage;
