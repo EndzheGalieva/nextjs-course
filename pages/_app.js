32e19ab112dc19745d0257492ce057214ca82505
Endzhe Galieva <endzhe91@gmail.com>
2021-04-18 19:46:18 +0300

    our target state and starting project

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 
-import '../styles/globals.css'
 import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
+import '../styles/globals.css'
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
@@ -12,6 +13,7 @@ function MyApp({Component, pageProps}) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test." status="error" />
     </Layout>
   );
 }
